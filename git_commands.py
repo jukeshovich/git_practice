15e918b029713ea030f998958309ee4efa0b5342
Erlan <erlan.jukeshovich@gmail.com>
2023-09-12 19:21:12 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,31 @@
+git init - creating file with .git
+
+git remote add (name url) вместо того,
+что находится в скобках вставляем либо https  либо ssh ссылку
+ДОбавляет удаленный репозиторий на каком то сервере
+
+git pull origin master - стягивание изменений с какой либо ветки
+
+git status - показывает статус файла(какие изменены добавлены и так далее)
+
+git add - добавляет файлы в индекс
+
+git add (name file)
+
+git add . - добавляет все файлы 
+
+git commit -m 'comment' - добавляет все файлы в индексе во внутр. базу данных
+
+git branch
+
+git branch (name branch)
+
+git checkout (name branch)
+
+git push (name branch)
+
+git push origin master
+
+git reset (filename) 
+
+
